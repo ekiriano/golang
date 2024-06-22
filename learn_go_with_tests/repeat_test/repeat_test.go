7367352886eb91a30b26f5b6ca6c85659a1ec887
ekiriano <ekiriano34@gmail.com>
2024-06-22 11:14:56 +0700

    end repeat chapter

---
+++ learn_go_with_tests/repeat_test/repeat_test.go
@@ -0,0 +1,39 @@
+package iteration
+
+import "testing"
+
+func Repeat(characters string, numberOfTime int) string {
+	var repeated string
+	for i := 0; i < numberOfTime; i++ {
+		repeated += characters
+	}
+	return repeated
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func TestRepeat(t *testing.T) {
+
+	t.Run("inital test case", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("repeat10times", func(t *testing.T) {
+		repeated := Repeat("a", 10)
+		expected := "aaaaaaaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+}
